fb469d54d685728ad8deaf94e5ad6cb9bf4d0971
Naman Sood <mail@nsood.in>
2023-07-28 02:43:56 -0400

    use newest version of zsh theme
    Signed-off-by: Naman Sood <mail@nsood.in>

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 echo "Downloading zsh theme"
 mkdir -p ~/.oh-my-zsh/custom/themes/
-wget -xqO ~/.oh-my-zsh/custom/themes/aphrodite.zsh-theme https://github.com/win0err/aphrodite-terminal-theme/raw/6f8733782e7e31676897dd9fb036fa7e057553c6/aphrodite.zsh-theme
+wget -xqO ~/.oh-my-zsh/custom/themes/aphrodite.zsh-theme https://github.com/win0err/aphrodite-terminal-theme/raw/master/aphrodite.zsh-theme
 
 for f in ${DOTFILES}
 do
